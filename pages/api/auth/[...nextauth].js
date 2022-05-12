6c46deb477338fe496a6ce8c584164883c068801
Tiago das Graças Arrais <37849109+tiagogarrais@users.noreply.github.com>
2022-05-12 09:36:25 -0300

    Add sign in with email and remove Gmail
    O login utilizando o Gmail foi removido e foi adicionado o login utilizando a autenticação por e-mail, portanto qualquer e-mail agora pode ser utilizado para fazer login no site.

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -1,12 +1,22 @@
 import NextAuth from "next-auth"
-import GoogleProvider from "next-auth/providers/google"
+import EmailProvider from "next-auth/providers/email"
+import { MongoDBAdapter } from "@next-auth/mongodb-adapter"
+import clientPromise from "../../../lib/mongodb"
 
 export default NextAuth({
   // Configure one or more authentication providers
   providers: [
-    GoogleProvider({
-        clientId: process.env.GOOGLE_CLIENT_ID,
-        clientSecret: process.env.GOOGLE_CLIENT_SECRET
-      })
+      EmailProvider({
+        server: {
+          host: process.env.EMAIL_SERVER_HOST,
+          port: process.env.EMAIL_SERVER_PORT,
+          auth: {
+            user: process.env.EMAIL_SERVER_USER,
+            pass: process.env.EMAIL_SERVER_PASSWORD
+          }
+        },
+        from: process.env.EMAIL_FROM
+      }),
   ],
+  adapter: MongoDBAdapter(clientPromise),
 })
